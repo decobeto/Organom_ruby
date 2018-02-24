28f5f340041aa23bdfe3cf8add8c126b0cb1ddfc
André Roberto <32197501+decobeto@users.noreply.github.com>
2018-02-23 21:48:29 -0300

    Add files via upload

---
+++ abaco.rb
@@ -0,0 +1,73 @@
+def primeira_pergunta
+  puts "Digite um número: "
+  @a = gets.chomp.to_f
+  puts "Digite o operador: "
+  @b = gets.chomp
+  puts "Digite outro número: "
+  @c = gets.chomp.to_f
+end
+
+def segunda_pergunta
+  puts "Digite outro operador: "
+  @b = gets.chomp
+  puts "Digite mais um número: "
+  @a = gets.chomp.to_f
+end
+  
+def engine
+  if (@contador == 0)
+    case (@b) 
+      when "+"
+      @d = @a + @c
+      puts "Resultado: #{@d}"
+      when "-"
+      @d = @a - @c
+      puts "Resultado: #{@d}"
+      when "/"
+      @d = @a / @c
+      puts "Resultado: #{@d}"
+      when "*"
+      @d = @a * @c
+      puts "Resultado: #{@d}"
+      when "="
+      puts "Resultado: #{@d}" 
+    end
+    @resultado << @d
+  elsif (@contador >= 1)
+    case (@b) 
+      when "+"
+      @d = @d + @a 
+      puts "Resultado: #{@d}"
+      when "-"
+      @d = @d - @a
+      puts "Resultado: #{@d}"
+      when "/"
+      @d = @d / @a
+      puts "Resultado: #{@d}"
+      when "*"
+      @d =  @d * @a
+      puts "Resultado: #{@d}"
+      when "="
+      puts "Resultado: #{@d}" 
+    end
+  end
+  @resultado << @d
+end
+
+def abaco
+  @contador = 0
+  @resultado = Array.new
+  primeira_pergunta
+  engine
+  while (@b != "=")
+    @contador = @contador + 1
+    segunda_pergunta
+    engine
+  end
+end
+
+abaco
+
+
+
+# resultado.reduce(:+) 
